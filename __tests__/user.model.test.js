8e799567df83e96c2bbd2dcd3e1f744d75659c67
urmybestfriend <c.goldentiger.c@gmail.com>
2019-08-22 14:06:50 +0600

    initial passing test

---
+++ __tests__/user.model.test.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+dbtype = process.env.dbtype ? 'mongo' : 'localhost'
+mongoose.connect('mongodb://' + dbtype + '/testUser', { useNewUrlParser: true });
+mongoose.connection.on('error', () => {
+    throw new Error(`unable to connect to database: `);
+})
+
+describe('initial test', ()=> {
+    it('runs successfully', ()=>{
+        expect(true).toEqual(true)
+    })
+})
+
+afterAll(async () => {
+    try {
+        await mongoose.connection.close();
+    }
+    catch (err) {
+        console.log(err);
+    }
+})
